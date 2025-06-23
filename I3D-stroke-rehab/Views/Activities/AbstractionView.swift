c7c475be2b0af904919f67a898ecfe48c79dc520
Maahir Garg <maahirrgarg@gmail.com>
2025-06-23 13:47:52 +0530

    Add delayed Recall

--- I3D-stroke-rehab/Views/Activities/AbstractionView.swift
+++ I3D-stroke-rehab/Views/Activities/AbstractionView.swift
@@ -25,7 +25,7 @@ struct AbstractionView: View {
                 Spacer()
                 
                 if manager.currentIndex >= tasks.count {
-                    CompletionView(completionText: "🎉 You’re done!", buttonText: "Next Task", destination: OrientationView())
+                    CompletionView(completionText: "🎉 You’re done!", buttonText: "Next Task", destination: DelayedRecallView())
                 } else {
                     let task = tasks[manager.currentIndex]
                     
